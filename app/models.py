8a5ba6b3118b5a10cb6bfa3629b67b65d6f085f0
Leonardo Queiroz <Leofilho229@gmail.com>
2025-04-01 17:07:04 -0300

    link  de relacionamento entre usuario, objetivos e subtarefa

--- app/models.py
+++ app/models.py
@@ -13,6 +13,7 @@ class Usuario(models.Model):
     Nome = models.CharField(max_length=100)
     E_mail = models.EmailField(unique=True)
     Senha = models.CharField(max_length=20)
+
     def __str__(self):
         return self.Nome
 
@@ -20,6 +21,8 @@ class Objetivo(models.Model):
     Nome = models.CharField(max_length=1000)
     Descrição = models.TextField()
     Estar_concluído = models.BooleanField(default=False)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name= 'Objetivos: ')
+
     def __str__(self):
         return self.Nome
     
@@ -27,6 +30,8 @@ class Subtarefa(models.Model):
     Nome = models.CharField(max_length=255)
     descrição = models.TextField()
     estar_concluído = models.BooleanField(default=False)
+    objetivo = models.ForeignKey(Objetivo, on_delete=models.CASCADE, related_name='Subtarefas: ')
+
     def __str__(self):
         return self.Nome
 
