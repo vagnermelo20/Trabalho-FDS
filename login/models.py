a0d99ec4b6fd48c66197c4f9891d008303ea172e
David-git123 <dmpo@cesar.school>
2025-04-03 20:01:45 -0300

    Fazendo mudança dos models

--- login/models.py
+++ login/models.py
@@ -17,22 +17,5 @@ class Usuario(models.Model):
     def __str__(self):
         return self.Nome
 
-class Objetivo(models.Model):
-    Nome = models.CharField(max_length=1000)
-    Descrição = models.TextField()
-    Estar_concluído = models.BooleanField(default=False)
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name= 'Objetivos')
-
-    def __str__(self):
-        return self.Nome
-    
-class Subtarefa(models.Model):
-    Nome = models.CharField(max_length=255)
-    descrição = models.TextField()
-    estar_concluído = models.BooleanField(default=False)
-    objetivo = models.ForeignKey(Objetivo, on_delete=models.CASCADE, related_name='Subtarefas')
-
-    def __str__(self):
-        return self.Nome
 
 
