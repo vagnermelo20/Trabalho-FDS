aab0c8fe095631297d582aa06cb6777435d7f400
David-git123 <dmpo@cesar.school>
2025-04-03 19:40:34 -0300

    Transferência das views

--- app/views.py
+++ app/views.py
@@ -27,43 +27,4 @@ class CriarUsuarioView(View):
         messages.success(request, f'Usuário "{nome}" criado com sucesso!')
         return redirect('criar_usuario')
     
-class CriarObjetivoView(View):
-    def get(self, request):
-        return HttpResponse('criar_objetivo.html')
 
-    def post(self, request):
-        nome_objetivo = request.POST.get('nome_objetivo')
-        descricao_objetivo = request.POST.get('descricao_objetivo')
-        subtarefas_nomes = request.POST.getlist('subtarefa_nome')
-        subtarefas_descricoes = request.POST.getlist('subtarefa_descricao')
-
-        usuario = Usuario.objects.first()
-
-        if not nome_objetivo:
-            messages.error(request, 'É necessário preencher o nome do objetivo.')
-            return redirect('criar_objetivo')
-
-        objetivo = Objetivo.objects.create(
-            Nome=nome_objetivo,
-            Descrição=descricao_objetivo,
-            usuario=usuario
-        )
-
-        count = 0
-        for i in range(len(subtarefas_nomes)):
-            nome = subtarefas_nomes[i]
-            descricao = subtarefas_descricoes[i]
-
-            if nome.strip():
-                Subtarefa.objects.create(
-                    Nome=nome,
-                    descrição=descricao,
-                    objetivo=objetivo
-                )
-                count += 1
-
-        messages.success(
-            request,
-            f'Objetivo "{nome_objetivo}" foi criado com sucesso com {count} subtarefa(s).'
-        )
-        return redirect('criar_objetivo')
