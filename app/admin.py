185b2c7b87af1dacb4755c6c8e5ccaf4fcf7294c
Vagner Montenegro de Melo <vagnermontenegro20@gmail.com>
2025-04-04 13:51:35 -0300

    deletei app

--- app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from app.models import Usuario, Objetivo, Subtarefa
-
-admin.site.register(Usuario)
-admin.site.register(Objetivo)
-admin.site.register(Subtarefa)
--- app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from app.views import CriarUsuarioView, CriarObjetivoView
-
-urlpatterns = [
-    
-    path('', CriarUsuarioView.as_view(), name='criar_usuario'),
-    path('objetivo/criar_objetivo/', CriarObjetivoView.as_view(), name='criar_objetivo'),
-]
-
