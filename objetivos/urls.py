ea0a6a4f9781f05f78dca3d04297af2701a5750a
Vagner Montenegro de Melo <vagnermontenegro20@gmail.com>
2025-04-05 16:42:32 -0300

    adicionado as urls de editar e de deletar

--- objetivos/urls.py
+++ objetivos/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from objetivos.views import  CriarObjetivoView,VisualizarObjetivosView
+from objetivos.views import CriarObjetivoView, DeletarObjetivoView, EditarObjetivoView, VisualizarObjetivosView
 
 urlpatterns = [
-    path('',VisualizarObjetivosView.as_view(),name='visualizar_objetivos'),
-    path('criar_objetivo/',CriarObjetivoView.as_view(),name='visualizar_objetivo')
+    path('', VisualizarObjetivosView.as_view(), name='visualizar_objetivos'),
+    path('criar_objetivo/', CriarObjetivoView.as_view(), name='criar_objetivo'),  # Corrigido o nome da URL
+    path('editar_objetivo/<int:objetivo_id>/', EditarObjetivoView.as_view(), name='editar_objetivo'),
+    path('deletar_objetivo/<int:objetivo_id>/', DeletarObjetivoView.as_view(), name='deletar_objetivo'),
 ]
 
