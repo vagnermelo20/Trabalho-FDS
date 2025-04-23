b18a1661dedc198747708603ba91bccff73618dc
Tomás Jamacaru <jamacarutomas78@gmail.com>
2025-04-22 21:02:40 -0300

    Delete Trabalho-FDS directory

--- Trabalho-FDS/login/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from login.views import CriarUsuarioView,LoginView
-
-urlpatterns = [
-    path('', LoginView.as_view(), name='logar'),
-    path('criar_usuario/',CriarUsuarioView.as_view(),name='criar_usuario'),
-    
-]
-
