36c30bdf815638507bd37f726d442c9b99707f32
Vagner Montenegro de Melo <vagnermontenegro20@gmail.com>
2025-04-29 13:48:25 -0300

    deixei falado o banco de dados

--- projeto/settings.py
+++ projeto/settings.py
@@ -31,7 +31,18 @@ DEBUG = os.getenv('DEBUG', 'False').lower() in ['true', '1', 't']
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(' ')
 #CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(' ')
 
-# Banco de Dados
+# Banco de Dados tradicional para mexer no codigo
+'''DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}'''
+
+
+# Banco de Dados pra deixar quando estiver em produção. 
+#LEMBRAR DE NÃO SUBIR O DB.SQLITE3 DO PROJETO NO GITHUB
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
