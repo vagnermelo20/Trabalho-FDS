b496769b4f643e6ab2b9e0105cf20813ee41f336
Vagner Montenegro de Melo <vagnermontenegro20@gmail.com>
2025-04-29 12:47:21 -0300

    consertando o banco de dados

--- projeto/settings.py
+++ projeto/settings.py
@@ -35,7 +35,7 @@ ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(' ')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/home/database/db.sqlite3',
     }
 }
 
