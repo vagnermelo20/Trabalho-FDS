4d6a8a6767a87aff906a27b8c9501126e6648364
Daniel <moongames12345@gmail.com>
2025-05-11 20:26:05 -0300

    .

--- objetivos/admin.py
+++ objetivos/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from objetivos.models import  Objetivo, Subtarefa
 
-admin.site.register(Objetivo)
+admin.site.register(Objetivo) 
+
 admin.site.register(Subtarefa)
