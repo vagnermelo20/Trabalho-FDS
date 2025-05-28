8baeb811248539fffe4c07843672335792cf7538
David-git123 <dmpo@cesar.school>
2025-05-28 13:50:38 -0300

    Registrando o banco de dados no admin

--- objetivos/admin.py
+++ objetivos/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from objetivos.models import  Objetivo, Subtarefa
+from objetivos.models import  Objetivo, Subtarefa,Grupos,Tarefas_grupos,Participantes_grupos
 
 admin.site.register(Objetivo) 
 
 admin.site.register(Subtarefa)
+
+admin.site.register(Grupos)
+admin.site.register(Tarefas_grupos)
+admin.site.register(Participantes_grupos)
